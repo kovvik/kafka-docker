2acf20d4289e0a88c5429fc6904df819a7546d09
Viktor Kovacs <kovvik@gmail.com>
2024-04-13 22:46:50 +0200

    entrypoint.sh: kraf controller config

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -37,8 +37,10 @@ then
 fi
 
 # Set config
-cat << EOF > /etc/server.properties
+cat << EOF > /usr/local/kafka/config/server.properties
 broker.id=${KAFKA_BROKER_ID}
+process.roles=broker,controller
+controller.quorum.voters=${KAFKA_CONTROLLER_QUORUM_VOTERS:-1@localhost:9093}
 num.network.threads=${KAFKA_NUM_NETWORK_THREADS:-3}
 num.io.threads=${KAFKA_IO_THREADS:-8}
 socket.send.buffer.bytes=${KAFKA_SOCKET_SEND_BUFFER_BYTES:-102400}
@@ -55,9 +57,10 @@ log.retention.hours=${KAFKA_LOG_RETENTION_HOURS:-120}
 log.segment.bytes=${KAFKA_LOG_SEGMENT_BYTES:-1048576}
 log.retention.check.interval.ms=${KAFKA_LOG_RETENTION_CHECK_INTERVAL_MS:-300000}
 group.initial.rebalance.delay.ms=${KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS:-0}
-listeners=${KAFKA_LISTENERS:-INTERNAL://0.0.0.0:9092}
-advertised.listeners=${KAFKA_ADVERTISED_LISTENERS:-INTERNAL://0.0.0.0:9092}
-listener.security.protocol.map=${KAFKA_LISTENER_SECURITY_PROTOCOL_MAP:-INTERNAL:PLAINTEXT}
+listeners=${KAFKA_LISTENERS:-INTERNAL://:9092,CONTROLLER://:9093}
+controller.listener.names=CONTROLLER
+advertised.listeners=${KAFKA_ADVERTISED_LISTENERS:-INTERNAL://:9092}
+listener.security.protocol.map=${KAFKA_LISTENER_SECURITY_PROTOCOL_MAP:-INTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT}
 inter.broker.listener.name=${KAFKA_INTER_BROKER_LISTENER_NAME:-INTERNAL}
 ssl.keystore.password=${KAFKA_SSL_KEYSTORE_PASSWORD}
 ssl.keystore.location=${KAFKA_SSL_KEYSTORE_LOCATION}
@@ -71,7 +74,13 @@ ssl.endpoint.identification.algorithm=
 EOF
 
 # Log config
-cat /etc/server.properties
+cat /usr/local/kafka/config/server.properties
+
+if [ ! $(cat /var/lib/kafka/kafka-logs/meta.properties 2>/dev/null | grep ${KAFKA_CLUSTER_ID}) ]
+then
+    echo "Formatting storage..."
+    /usr/local/kafka/bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c /usr/local/kafka/config/server.properties
+fi
 
 # Start kafka
-/usr/local/kafka/bin/kafka-server-start.sh /etc/server.properties
+/usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties
